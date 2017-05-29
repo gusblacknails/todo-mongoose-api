5fdf4720b734635161e1e4e37534897ca8647e14
Gus Escola <camagrock@gmail.com>
2017-05-29 12:14:11 +0200

    fixed paths_3

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require ('path')
-const app = express()
+
 
 const getPass = require  ('./routes/middlewares/getPass')
 const routerTasks = require('./routes/tasks')
@@ -13,18 +13,26 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const dbUrl = process.env.DB_URL
 const PORT = process.env.PORT
+const app = express()
 
 
 
 mongoose.Promise = Promise
 mongoose.connect(dbUrl)
+// app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", 'pug')
 app.set('views',path.join(__dirname, 'views'))
-// app.use(express.static('public'))
-app.use(express.static(__dirname + '/public'));
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  require('debug')('body-parser')(req.body)
+  next()
+})
 app.use('/middleware', getPass)
 app.use('/tasks', routerTasks)
 app.use('/task', routerTask)
